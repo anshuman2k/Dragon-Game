7579564a454baa5da6554aac2de45155def76ca2
anshuman2k <51052530+anshuman2k@users.noreply.github.com>
2021-06-07 19:23:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ setInterval(() => {
         }, 1000);
         cross=false;
     }
-    else if (cross && offsetX < 300) {
+    else if (cross && offsetX < 150) {
         score += 1;
         updateScore(score);
         cross = false;
